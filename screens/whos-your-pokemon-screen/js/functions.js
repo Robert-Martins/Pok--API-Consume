19fffedb9ce19dcd7f472f3c2038c279a49e8fdc
Robert-Martins <robertcantares@gmail.com>
2022-05-08 22:30:30 -0300

    Developing Version 1.0.0

--- screens/whos-your-pokemon-screen/js/functions.js
+++ screens/whos-your-pokemon-screen/js/functions.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
 
     //Funções para geração de novos Pokémons
 
-    var pokemonData = ([]) //Constante para guardar os objeto de dados do Pokémon
+    var pokemonData = ([]) //Variável para guardar os objeto de dados do Pokémon
+
+    var didRequest = false
 
     const changeExhibitedData = (name, image) =>{
         $('#poke-img').fadeOut()
@@ -29,33 +31,53 @@ $(document).ready(function(){
     }
 
     const changeExhibitedInfo = (info) =>{
-
+        $('#pokedex-id').html('#'+info.id+' - '+info.name)
+        $('#pokedex-img').attr('src',info.sprites.front_default)
     }
 
     const changeExhibitedMoves = (moves) =>{
+        el = $('.pokedex-moves')
+        el.html('')
+        let max = moves.length
+        for(let count = 1; count <= 4; count++){
+            let val = randomNumberGeneration(max, 0)
+            el.append('<h4>Move '+count+': '+moves[val].move.name+'</h4>')
+        }
 
     }
 
     const changeExhibitedTypes = (types) =>{
-
+        el = $('.pokedex-types')
+        let typeCount = 0;
+        el.html('')
+        types.map(function(val){
+            typeCount++
+            el.append('<h4>Type '+typeCount+': '+val.type.name+'</h4>')
+        })
     }
 
     const changeExhibitedStats = (stats) =>{
-        var counter = 0;
+        el = $('.pokedex-stats')
+        let statCount = 0;
+        el.html('')
         stats.map(function(val){
-            $('#stat-'+counter).html(val.base_stat)
-            counter++;
+            statCount++
+            el.append('<h4>'+val.stat.name+': '+val.base_stat+'</h4>')
         })
-        //
     }
 
-    const randomNumberGeneration = () =>{
-        return Math.floor(Math.random() * (897 - 1) + 1);
+    const showPokedexBtn = () =>{
+        didRequest = true
+        $('#pokedex-btn').removeClass('disabled')
+    }
+
+    const randomNumberGeneration = (max, min) =>{
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
     const randomPokemonGeneration = () =>{
-        var randomNumber = randomNumberGeneration()
-        if(randomNumber >= 0 && randomNumber <= 600){
+        var randomNumber = randomNumberGeneration(897, 1)
+        if(randomNumber >= 0 && randomNumber <= 897){
             const url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
 
             fetch(url, {
@@ -64,8 +86,8 @@ $(document).ready(function(){
                 return response.json()
             }).then(data =>{
                 changeExhibitedData(data.name, data.sprites.front_default)
-                changeExhibitedStats(data.stats)
                 getPokémonData(data)
+                if(didRequest == false) showPokedexBtn()
             })
         }
 
@@ -77,31 +99,94 @@ $(document).ready(function(){
 
     //Funções para criação de html da modal de pokédex
 
+    $('body').append(`
+    <div id="pokedex-modal-container" class="modal-container">
+        <div class="pokedex-modal">
+            <h3>POKÉDEX</h3>
+            <div class="container">
+                <div class="data-container">
+                    <h3>Pokémon</h3>
+                    <div class="pokedex-info">
+                        <h3 id="pokedex-id"></h3>
+                        <img id="pokedex-img" src=""/>
+                    </div><!--pokedex-moves-->
+                </div><!--data-container-->
+                <div class="data-container">
+                    <h3>Type</h3>
+                    <div class="pokedex-types text-style">
+                    
+                    </div><!--pokedex-types-->
+                </div><!--data-container-->
+                <div class="data-container">
+                    <h3>Stats</h3>
+                    <div class="pokedex-stats text-style">
+                    
+                    </div><!--pokedex-stats-->
+                </div><!--data-container-->
+                <div class="data-container">
+                    <h3>Moves</h3>
+                    <div class="pokedex-moves text-style">
+                    
+                    </div><!--pokedex-moves-->
+                </div><!--data-container-->
+                <div style="clear:both"></div>
+            </div><!--container-->
+        </div><!--pokedex-modal-->
+    </div><!--modal-container-->
+    `)
+
     //Funções para exibição de modal de pokédex
 
     const openPokedexModal = () =>{
-        changePokedexData()
+        $('body').css('overflow', 'hidden')
+        $('#pokedex-modal-container').fadeIn()
     }
 
-    $('pokedex-btn').click(function(){
+    const closePokedexModal = () =>{
+        $('body').css('overflow', 'auto')
+        $('#pokedex-modal-container').fadeOut()
+    }
 
+    $('#pokedex-btn').click(function(){
+        if(didRequest){
+            changePokedexData()
+            openPokedexModal()
+        }
+    })
+
+    $('#pokedex-modal-container').click(function(e){
+        if(!($(e.target).closest('.pokedex-modal').length > 0)){
+            closePokedexModal()
+        }
     })
 
     //Funções para inclusão de botões na modal de menu
 
-    $('body').append('<div id="menu-modal-container" class="modal-container"></div><!--modal-container-->')
-
-    $('#menu-modal-container').append('<div class="menu-modal"></div><!--menu-modal-->')
-
-    $('.menu-modal').append('<h1 style="margin-bottom:24px; ">MENU</h1>')
-
-    $('.menu-modal').append('<div style="margin-bottom: 12px"><a href="../../index.html"><h3>Home</h3></a></div>')
-
-    $('.menu-modal').append('<a href="../choose-a-pokemon-screen"><img src="../../../assets/icons/function-2-logo.png" /><h3>Choose Your Pokémon</h3></a>')
-
-    $('.menu-modal').append('<a href="../build-a-pokemon-team"><img src="../../../assets/icons/function-3-logo.png" /><h3>Build Your Team</h3></a>')
-
-    $('.menu-modal').append('<div style="margin-top: 16px"><a href=""><h3>Back to Projects</h3></a></div>')
+    $('body').append(`
+    <div id="menu-modal-container" class="modal-container">
+        <div class="menu-modal">
+            <h1 style="margin-bottom:24px; ">MENU</h1>
+            <div style="margin-bottom: 12px">
+                <a href="../../index.html">
+                    <h3>Home</h3>
+                </a>
+            </div>
+            <a href="../choose-a-pokemon-screen">
+                <img src="../../../assets/icons/function-2-logo.png" />
+                <h3>Choose Your Pokémon</h3>
+            </a>
+            <a href="../build-a-pokemon-team">
+                <img src="../../../assets/icons/function-3-logo.png" />
+                <h3>Build Your Team</h3>
+            </a>
+            <div style="margin-top: 16px">
+                <a href="">
+                    <h3>Back to Projects</h3>
+                </a>
+            </div>
+        </div><!--menu-modal-->
+    </div><!--modal-container-->
+    `)
 
     //Funções para exibição de modal de menu
 
